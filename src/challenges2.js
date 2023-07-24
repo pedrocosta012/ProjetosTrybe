355cc21da35293d2486c09c48d871734141cc0ce
Pedro Costa <pedrocostaugusto@gmail.com>
2023-07-24 15:48:38 -0300

    🦄 refactor(todos os desafios com erro de lint): aplica melhores práticas

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,74 +1,55 @@
+function moreThenThreeRepetitions(numbersArray) {
+  for (const num of numbersArray) {
+    if (numbersArray.filter((num2) => num === num2).length === 3) {
+      return true;
+    }
+  }
+}
+
+function isInvalidPhoneNumber(arrayPhoneNumber) {
+  if (arrayPhoneNumber.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  const tooManyRepetitions = moreThenThreeRepetitions(arrayPhoneNumber);
+  if (!arrayPhoneNumber.every((num) => num <= 9 && num >= 0) || tooManyRepetitions) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+}
+
+const mountPhoneNumber = (numbersArray) => numbersArray
+  .map((num, i) => {
+    if (i === 0) return ['(', num];
+    if (i === 1) return [num, ')', ' '];
+    if (i === 6) return [num, '-'];
+    return num;
+  }).flat().join('');
+
 // Desafio 11
 function generatePhoneNumber(arrayPhoneNumber) {
-  
-  if (arrayPhoneNumber.length !== 11) {
-    return "Array com tamanho incorreto.";
+  const invalidPhoneNumber = isInvalidPhoneNumber(arrayPhoneNumber);
+  if (invalidPhoneNumber) {
+    return invalidPhoneNumber;
   }
-
-  for (let erroNum of arrayPhoneNumber) {
-    if (erroNum < 0 || erroNum > 9) {
-      return "não é possível gerar um número de telefone com esses valores";
-    }
-  }
-
-  for (let repete1 of arrayPhoneNumber) {
-    let count = 0;
-    for (let repete2 of arrayPhoneNumber) {
-      if (repete1 === repete2) {
-        count++;
-      }
-      if (count === 3) {
-      return "não é possível gerar um número de telefone com esses valores";
-      }
-    }
-  }
-
-  let phoneNumber = '';
-
-  for (let i = 0; i < 11; i++) {
-    switch (i) {
-      case 0:
-        phoneNumber += '(';
-        break;
-      case 2:
-        phoneNumber += ') ';
-        break;
-      case 7:
-        phoneNumber += '-';
-        break;
-    }
-    phoneNumber += arrayPhoneNumber[i];
-  }
-  return phoneNumber;
+  return mountPhoneNumber(arrayPhoneNumber);
 }
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
-  if (((lineA + lineB) > lineC) && (Math.abs(lineA - lineB) < lineC)) {
-    return true;
-  } else if (lineA + lineC > lineB && Math.abs(lineA - lineC) < lineB) {
-    return true;
-  } else if ((lineC + lineB) > (lineA) && (Math.abs(lineC - lineB)) < (lineA)) {
-    return true;
-  }
-  return false;
+  return ((lineC + lineB) > lineA) && (Math.abs(lineC - lineB) < lineA);
 }
 
 // Desafio 13
 function hydrate(biritas) {
-  let num = biritas.match(/\d+/g);
-  
-  if (num == 1) {
-    return num + ' copo de água';
-  } else {
+  const num = biritas.match(/\d+/g);
 
-    let agua = 0;
-  
-    for (let a of num) {
-      agua += Number.parseInt(a);
-    }
-    return agua + ' copos de água';
+  if (num.length === 1 && num[0] === '1') {
+    return `${num[0]} copo de água`;
   }
+  let agua = 0;
+  for (let a of num) {
+    agua += Number.parseInt(a, 10);
+  }
+  return `${agua} copos de água`;
 }
 
 module.exports = {

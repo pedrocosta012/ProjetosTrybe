fa4f90e2b2904bea1d7097a1cade2934776567be
Pedro Costa <pedrocostaugusto@gmail.com>
2023-08-03 06:46:52 -0300

    chore: apply lint rules

--- script.js
+++ script.js
@@ -1,123 +1,123 @@
 /* eslint-disable indent */
 window.onload = function () {
-    function newColors() {
-        let allColors = document.getElementById('color-palette').children;
-        let count;
+	function newColors() {
+		let allColors = document.getElementById('color-palette').children;
+		let count;
 
-        if (localStorage.getItem('count') != undefined) {
-            count = localStorage.getItem('count');
-            count++;
-            localStorage.setItem('count', count);
-            for (let color of allColors) {
-                if (allColors[0] == color) {
-                    color.style.backgroundColor = 'rgb(0, 0, 0)';
-                } else {
-                    let r = Math.floor(Math.random() * 255) + 1;
-                    let g = Math.floor(Math.random() * 255) + 1;
-                    let b = Math.floor(Math.random() * 255) + 1;
-                    color.style.backgroundColor = 'rgb(' + r + ', ' + g + ', ' + b + ')';
-                }
-            }
-        } else {
-            localStorage.setItem('count', 0);
-            count = 0;
-        }
-    }
+		if (localStorage.getItem('count') != undefined) {
+			count = localStorage.getItem('count');
+			count++;
+			localStorage.setItem('count', count);
+			for (let color of allColors) {
+				if (allColors[0] == color) {
+					color.style.backgroundColor = 'rgb(0, 0, 0)';
+				} else {
+					let r = Math.floor(Math.random() * 255) + 1;
+					let g = Math.floor(Math.random() * 255) + 1;
+					let b = Math.floor(Math.random() * 255) + 1;
+					color.style.backgroundColor = 'rgb(' + r + ', ' + g + ', ' + b + ')';
+				}
+			}
+		} else {
+			localStorage.setItem('count', 0);
+			count = 0;
+		}
+	}
 
-    newColors();
+	newColors();
 
-    function sizeBoard() {
-        let size = 5;
-        let board = document.getElementById('pixel-board');
+	function sizeBoard() {
+		let size = 5;
+		let board = document.getElementById('pixel-board');
 
-        for (let i = 0; i < size; i++) {
-            let div = document.createElement('div');
-            for (let j = 0; j < size; j++) {
-                let pixel = document.createElement('div');
-                pixel.className = 'pixel';
-                div.appendChild(pixel);
-            }
-            board.appendChild(div);
-        }
+		for (let i = 0; i < size; i++) {
+			let div = document.createElement('div');
+			for (let j = 0; j < size; j++) {
+				let pixel = document.createElement('div');
+				pixel.className = 'pixel';
+				div.appendChild(pixel);
+			}
+			board.appendChild(div);
+		}
 
-        document.getElementById('generate-board').addEventListener('click', function () {
-            let size = parseInt(document.getElementsByTagName('input')[0].value);
-            let board = document.getElementById('pixel-board');
-            let boardChildren = document.querySelectorAll('#pixel-board div');
-            let lastsize = size;
+		document.getElementById('generate-board').addEventListener('click', function () {
+			let size = parseInt(document.getElementsByTagName('input')[0].value);
+			let board = document.getElementById('pixel-board');
+			let boardChildren = document.querySelectorAll('#pixel-board div');
+			let lastsize = size;
 
-            if (size < 5) {
-                size = 5;
-            } else if (size > 50) {
-                size = 50;
-            }
+			if (size < 5) {
+				size = 5;
+			} else if (size > 50) {
+				size = 50;
+			}
 
-            if (document.getElementsByTagName('input')[0].value === '') {
-                alert('Board inválido!');
-                size = lastsize;
-            }
+			if (document.getElementsByTagName('input')[0].value === '') {
+				alert('Board inválido!');
+				size = lastsize;
+			}
 
-            for (let son of boardChildren) {
-                if (son.classList == '') {
-                    board.removeChild(son);
-                }
-            }
+			for (let son of boardChildren) {
+				if (son.classList == '') {
+					board.removeChild(son);
+				}
+			}
 
-            for (let i = 0; i < size; i++) {
-                let div = document.createElement('div');
-                for (let j = 0; j < size; j++) {
-                    let pixel = document.createElement('div');
-                    pixel.className = 'pixel';
-                    div.appendChild(pixel);
-                }
-                board.appendChild(div);
-            }
+			for (let i = 0; i < size; i++) {
+				let div = document.createElement('div');
+				for (let j = 0; j < size; j++) {
+					let pixel = document.createElement('div');
+					pixel.className = 'pixel';
+					div.appendChild(pixel);
+				}
+				board.appendChild(div);
+			}
 
-            painter();
+			painter();
 
-        });
-    }
+		});
+	}
 
-    sizeBoard();
+	sizeBoard();
 
-    document.getElementById('first').className = 'color selected';
+	document.getElementById('first').className = 'color selected';
 
-    function colorSelect() {
-        let colors = document.getElementsByClassName('color');
-        for (let color of colors) {
-            color.addEventListener('click', function () {
-                let items = document.getElementsByClassName('color');
-                for (let item of items) {
-                    item.setAttribute('class', 'color');
-                }
-                event.target.setAttribute('class', 'color selected');
-            });
-        }
-    }
+	function colorSelect() {
+		let colors = document.getElementsByClassName('color');
+		for (let color of colors) {
+			color.addEventListener('click', function (event) {
+				let items = document.getElementsByClassName('color');
+				for (let item of items) {
+					item.setAttribute('class', 'color');
+				}
+				event.target.setAttribute('class', 'color selected');
+			});
+		}
+	}
 
-    colorSelect();
+	colorSelect();
 
-    function painter() {
-        let pixels = document.getElementsByClassName('pixel');
-        for (let pixel of pixels) {
-            pixel.addEventListener('click', function () {
-                let idColor = document.getElementsByClassName('selected')[0].getAttribute('id');
-                event.target.setAttribute('id', idColor);
-            });
-        }
-    }
+	function painter() {
+		let pixels = document.getElementsByClassName('pixel');
+		for (let pixel of pixels) {
+			pixel.addEventListener('click', function () {
+				let idColor = document.getElementsByClassName('selected')[0].getAttribute('id');
+				event.target.setAttribute('id', idColor);
+			});
+		}
+	}
 
-    painter();
+	painter();
 
-    function colorClear() {
-        document.getElementById('clear-board').addEventListener('click', function () {
-            let pixels = document.getElementsByClassName('pixel');
-            for (let pixel of pixels) {
-                pixel.removeAttribute('id');
-            }
-        });
-    }
+	function colorClear() {
+		document.getElementById('clear-board').addEventListener('click', function () {
+			let pixels = document.getElementsByClassName('pixel');
+			for (let pixel of pixels) {
+				pixel.removeAttribute('id');
+			}
+		});
+	}
 
-    colorClear();
+	colorClear();
 
 };
